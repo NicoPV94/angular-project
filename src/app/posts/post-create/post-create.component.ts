f984c07a03bcef27b43d246fce0a084eccddbc79
Nicolas Portocarrero <xilotepet@gmail.com>
2020-01-13 12:03:39 -0500

    Added getting user input functionality on the post-create component

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,6 +1,15 @@
 import { Component } from '@angular/core';
 
-@Component({})
+@Component({
+    selector: 'app-post-create',
+    templateUrl: './post-create.component.html'
+})
 export class PostCreateComponent {
+    enteredValue = '';
+    newPost = 'No Content'; //Create new property, you don't need to use "const, let or var" to declare it.
+
+    onAddPost() {
+        this.newPost = this.enteredValue ;
+    }
 
 }
